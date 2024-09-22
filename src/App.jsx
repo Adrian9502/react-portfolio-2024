e1022095a43dd76b04be956c72e61c4e3e606ae0
Adrian <bontojohnadrian@gmail.com>
2024-09-22 11:03:16 +0800

    add router and link for header img

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./Components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Certification from "./Components/Certification";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
@@ -9,7 +10,9 @@ function App() {
     <main className="px-5 relative md:px-14 xl:px-52 ">
       <GlowMouse />
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <About />
     </main>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,25 @@
+import { Link } from "react-router-dom";
 import "./Header/header.css";
+
 export default function Header() {
   return (
     <header className="sticky rounded-bl-lg rounded-br-lg top-0 z-10 p-5 flex items-center justify-between">
       <section>
-        <img className="w-24" src="/Header/header-logo.png" alt="Header Logo" />
+        <Link to="/">
+          <img
+            className="w-24"
+            src="/Header/header-logo.png"
+            alt="Header Logo"
+          />
+        </Link>
       </section>
       <div className="text-slate-300 text-sm lg:text-lg flex flex-nowrap gap-2">
-        <nav className="nav-item hover:text-custom-cyan">Home</nav>
-        <div className="nav-item hover:text-custom-cyan">Certification</div>
+        <nav className="nav-item hover:text-custom-cyan">
+          <Link to="/">Home</Link>
+        </nav>
+        <div className="nav-item hover:text-custom-cyan">
+          <Link to="/certification">Certification</Link>
+        </div>
         <div className="nav-item hover:text-custom-cyan">Projects</div>
         <div className="nav-item hover:text-custom-cyan">Skills</div>
         <div className="nav-item hover:text-custom-cyan">Contact</div>
